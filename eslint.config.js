61652ed80fd045824a890625523e84a609a2fc80
danijeldzakula <danijeldzakula1991@gmail.com>
2024-12-15 17:40:24 +0100

    feat: (eslint) configuration v1

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,24 @@
-export default [
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ['eslint:recommended', 'next'],
+  }),
   {
     files: ['*.ts', '*.tsx', '*.mjs', '*.cjs'],
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
     },
   },
+  {
+    ignores: ['.next/**/*'], // Ignoriši `.next` direktorijum i njegove poddirektorijume
+  },
 ];
+
+export default eslintConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
+import React from 'react';
 
 import { Layout } from '@/components/content';
 
