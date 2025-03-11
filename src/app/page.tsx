8d4dcf098e7196ec8bdb8ce308d4598980b160fc
danijeldzakula <danijeldzakula1991@gmail.com>
2025-03-11 12:31:24 +0100

    chore: 🧹 change name of employee services function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,12 @@ import { API } from '@/services/api.service';
 import EmployeeColumns from './_components/employee-columns';
 
 export default async function Home() {
-  const { data } = await API.employee.retriveAllEmployees();
+  const { data } = await API.employee.retrieveAllEmployees();
 
   return (
     <Section>
       <Container>
-        <DataTable
-          data={data}
-          columns={EmployeeColumns({ userRole: 'Super Admin' })}
-        >
+        <DataTable data={data} columns={EmployeeColumns({ userRole: 'Admin' })}>
           <p className="bg-green-50 p-4 text-center">Data not found.</p>
         </DataTable>
       </Container>
--- src/services/employees.service.ts
+++ src/services/employees.service.ts
@@ -4,17 +4,16 @@ import { z } from 'zod';
 
 import { axiosClient } from '@/lib/axios-client';
 
-export async function retriveAllEmployees() {
+export async function retrieveAllEmployees() {
   try {
     const response = await axiosClient.get('/employees');
     return response.data;
   } catch (error) {
     if (error instanceof z.ZodError) {
-      console.log('🚀 ~ Zod Validation Error:', error.errors);
+      console.error('🚀 ~ Zod Validation Error:', error.errors);
     } else {
-      console.log('🚀 ~ retriveAllEmployees ~ error:', error);
+      console.error('🚀 ~ retrieveAllEmployees ~ error:', error);
     }
-    // throw new Error('Failed to retrieve employees or validation error');
     throw error;
   }
 }
